48b435a85e374c29dcc597573377e6e57d1b0eba
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-14 23:24:10 +0100

    Add About route

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ import {
 
 // App Components
 import Home from "./Home";
+import About from "./About";
 
 const App = () => (
     <BrowserRouter>
         <div className="container">
             <Route path="/" component={Home}/>
+            <Route path="/about" component={About} />
         </div>
     </BrowserRouter>
 );
