2b8161941e550c1440d8b4bc4041a97a11156adb
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-14 23:45:50 +0100

    Add props to About route

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,17 @@ import {
 import Header from "./Header";
 import Home from "./Home";
 import About from "./About";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
 
 const App = () => (
     <BrowserRouter>
         <div className="container">
             <Header />
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" render={ () => <About title='About' />} />
+            <Route path="/teachers" component={Teachers} />
+            <Route path="/courses" component={Courses} />
         </div>
     </BrowserRouter>
 );
