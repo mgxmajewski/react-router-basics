e97233c8781014cc9e45d6002a33b9ce6d0fae74
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-15 02:14:31 +0100

    Add url parameter to render name of a teacher

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Featured = () => {
-
-  return (
-    <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong></strong>, a teacher who loves teaching courses about <strong></strong>!</p>
-    </div>
-  );
+const Featured = ({match}) => {
+    let name = match.params.name;
+        return (
+        <div className="main-content">
+            <h2>{name}</h2>
+            <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong></strong>!</p>
+        </div>
+    );
 }
 
 export default Featured;
