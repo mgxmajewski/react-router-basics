45c11e09e89ee440a46d53738d2fc67c74411818
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-15 02:19:08 +0100

    Add topic parameter

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ const App = () => (
                 <Route exact path="/" component={Home} />
                 <Route path="/about" render={ () => <About title='About' />} />
                 <Route exact path="/teachers" component={Teachers} />
-                <Route path="/teachers/:name" component={Featured} />
+                <Route path="/teachers/:topic/:name" component={Featured} />
                 <Route path="/courses" component={Courses} />
                 <Route component={NotFound} />
             </Switch>
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from 'react'
 
 const Featured = ({match}) => {
-    let name = match.params.name;
+    let name = match.params.name
+    let topic = match.params.topic
         return (
         <div className="main-content">
             <h2>{name}</h2>
-            <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong></strong>!</p>
+            <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong>{topic}</strong>!</p>
         </div>
     );
 }
 
-export default Featured;
+export default Featured
